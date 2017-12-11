6438e9e496f612c3d2d13e5c450cb89dafe485a2
YangWanjun <ywjsailor@gmail.com>
2017-12-12 01:27:59 +0900

    仮契約情報変更

--- contract/admin.py
+++ contract/admin.py
@@ -98,3 +98,87 @@ class ContractAdmin(BaseAdmin):
     list_display = ('contractor', 'parking_lot', 'parking_position', 'contract_date', 'start_date', 'end_date')
     list_display_links = ('contractor',)
     inlines = (ContractPaymentInline,)
+
+
+@admin.register(models.TempContractor)
+class TempContractorAdmin(BaseAdmin):
+    icon = '<i class="material-icons">nature_people</i>'
+    list_display = ('name', 'category', 'address1', 'address2')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('category',),
+                ('name', 'kana',),
+                'post_code',
+                ('address1', 'address2'),
+                ('tel', 'fax',),
+                'email',
+                'comment',
+            )
+        }),
+        ("個人情報", {
+            'classes': ('collapse',),
+            'fields': (
+                ('personal_gender', 'personal_birthday', ),
+            )
+        }),
+        ("法人情報", {
+            'classes': ('collapse',),
+            'fields': (
+                ('corporate_business_type', ),
+                ('corporate_president', 'corporate_web_site'),
+                ('corporate_staff_name', 'corporate_staff_department', 'corporate_staff_position', 'corporate_extension'),
+                ('corporate_capital', 'corporate_turnover'),
+            )
+        }),
+        ("勤務先", {
+            'classes': ('collapse',),
+            'fields': (
+                'workplace_name',
+                'workplace_post_code',
+                ('workplace_address1', 'workplace_address2'),
+                ('workplace_tel', 'workplace_fax'),
+            )
+        }),
+        ("連絡先", {
+            'classes': ('collapse',),
+            'fields': (
+                ('contact_name', 'contact_kana',),
+                'contact_post_code',
+                ('contact_address1', 'contact_address2',),
+                ('contact_tel', 'contact_fax',),
+                'contact_relation',
+            )
+        }),
+        ("書類送付先", {
+            'classes': ('collapse',),
+            'fields': (
+                'delivery_type',
+                'delivery_honorific',
+                ('delivery_name', 'delivery_kana',),
+                'delivery_post_code',
+                ('delivery_address1', 'delivery_address2'),
+                ('delivery_tel', 'delivery_fax'),
+            )
+        }),
+        ("保証人", {
+            'classes': ('collapse',),
+            'fields': (
+                ('guarantor_name', 'guarantor_kana'),
+                'guarantor_birthday',
+                'guarantor_post_code',
+                ('guarantor_address1', 'guarantor_address2'),
+                ('guarantor_tel', 'guarantor_fax'),
+                'guarantor_relation',
+                'guarantor_comment',
+            )
+        })
+    )
+
+
+@admin.register(models.TempContract)
+class TempContractAdmin(BaseAdmin):
+    list_display = ('contractor', 'parking_lot', 'parking_position', 'contract_date', 'start_date', 'end_date')
+    list_display_links = ('contractor',)
