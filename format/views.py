a4876144371010053701e4a3ac0a827d9744b170
YangWanjun <ywjsailor@gmail.com>
2017-12-17 22:55:21 +0900

    申込書

--- format/views.py
+++ format/views.py
@@ -11,6 +11,8 @@ from parkinglot.models import ParkingLot
 from utils import constants, common
 from utils.django_base import BaseView, BaseTemplateView
 
+logger = common.get_ap_logger()
+
 
 # Create your views here.
 class UserOperationView(BaseTemplateView):
@@ -48,6 +50,7 @@ class SubscriptionConfirmView(BaseView):
             report_file.save()
             return JsonResponse({'error': False, 'message': '成功しました。'})
         except Exception as ex:
+            logger.error(ex)
             return JsonResponse({'error': True, 'message': str(ex)})
 
 
@@ -58,9 +61,76 @@ class SubscriptionView(BaseView):
         return HttpResponse(html)
 
     def post(self, request, *args, **kwargs):
-        report = get_object_or_404(models.ReportSubscription, pk=kwargs.get('report_id'))
-        parking_lot = get_object_or_404(ParkingLot, pk=kwargs.get('lot_id'))
-        contractor = get_object_or_404(TempContractor, pk=kwargs.get('contractor_id'))
+        # 車庫証明
+        rdo_receipt_yes = request.POST.get('rdo_receipt_yes')
+        rdo_receipt_no = request.POST.get('rdo_receipt_no')
+        # 駐車する車
+        txt_car_maker = request.POST.get('txt_car_maker')
+        txt_car_model = request.POST.get('txt_car_model')
+        txt_no_plate = request.POST.get('txt_no_plate')
+        txt_car_length = request.POST.get('txt_car_length')
+        txt_car_width = request.POST.get('txt_car_width')
+        txt_car_height = request.POST.get('txt_car_height')
+        txt_car_weight = request.POST.get('txt_car_weight')
+        # 希望契約開始日
+        txt_contract_start_date = request.POST.get('txt_contract_start_date')
+        # 希望契約期間
+        rdo_contract_period_long = request.POST.get('rdo_contract_period_long')
+        rdo_contract_period_short = request.POST.get('rdo_contract_period_short')
+        # 法人連絡先
+        txt_corporate_kana = request.POST.get('txt_corporate_kana')
+        txt_corporate_name = request.POST.get('txt_corporate_name')
+        txt_corporate_president = request.POST.get('txt_corporate_president')
+        txt_corporate_post1 = request.POST.get('txt_corporate_post1')
+        txt_corporate_post2 = request.POST.get('txt_corporate_post2')
+        txt_corporate_address1 = request.POST.get('txt_corporate_address1')
+        txt_corporate_address2 = request.POST.get('txt_corporate_address2')
+        txt_corporate_tel = request.POST.get('txt_corporate_tel')
+        txt_corporate_fax = request.POST.get('txt_corporate_fax')
+        txt_corporate_business_type = request.POST.get('txt_corporate_business_type')
+        # 法人（契約担当者）
+        txt_corporate_staff_kana = request.POST.get('txt_corporate_staff_kana')
+        txt_corporate_staff_name = request.POST.get('txt_corporate_staff_name')
+        txt_corporate_staff_email = request.POST.get('txt_corporate_staff_email')
+        txt_corporate_staff_tel = request.POST.get('txt_corporate_staff_tel')
+        txt_corporate_staff_fax = request.POST.get('txt_corporate_staff_fax')
+        txt_corporate_staff_phone = request.POST.get('txt_corporate_staff_phone')
+        # 法人（緊急連絡先）
+        txt_corporate_contact_tel = request.POST.get('txt_corporate_contact_tel')
+        txt_corporate_contact_name = request.POST.get('txt_corporate_contact_name')
+        txt_corporate_contact_relation = request.POST.get('txt_corporate_contact_relation')
+        # 個人連絡先
+        txt_personal_kana = request.POST.get('txt_personal_kana')
+        txt_personal_name = request.POST.get('txt_personal_name')
+        txt_personal_birthday = request.POST.get('txt_personal_birthday')
+        txt_personal_post1 = request.POST.get('txt_personal_post1')
+        txt_personal_post2 = request.POST.get('txt_personal_post2')
+        txt_personal_address1 = request.POST.get('txt_personal_address1')
+        txt_personal_address2 = request.POST.get('txt_personal_address2')
+        txt_personal_tel = request.POST.get('txt_personal_tel')
+        txt_personal_email = request.POST.get('txt_personal_email')
+        # 個人（勤務先）
+        txt_workplace_name = request.POST.get('txt_workplace_name')
+        txt_workplace_address1 = request.POST.get('txt_workplace_address1')
+        txt_workplace_address2 = request.POST.get('txt_workplace_address2')
+        txt_workplace_tel = request.POST.get('txt_workplace_tel')
+        txt_workplace_fax = request.POST.get('txt_workplace_fax')
+        # 個人（緊急連絡先）
+        txt_personal_contact_tel = request.POST.get('txt_personal_contact_tel')
+        txt_personal_contact_name = request.POST.get('txt_personal_contact_name')
+        txt_personal_contact_relation = request.POST.get('txt_personal_contact_relation')
+        # 媒介
+        chk_route_flier = request.POST.get('chk_route_flier')
+        chk_route_internet = request.POST.get('chk_route_internet')
+        chk_route_board = request.POST.get('chk_route_board')
+        chk_route_news = request.POST.get('chk_route_news')
+        chk_route_estate = request.POST.get('chk_route_estate')
+        chk_route_introduced = request.POST.get('chk_route_introduced')
+        chk_route_other = request.POST.get('chk_route_other')
+        txt_route_other = request.POST.get('txt_route_other')
+        # 順番待ち
+        rdo_waiting_yes = request.POST.get('rdo_waiting_yes')
+        rdo_waiting_no = request.POST.get('rdo_waiting_no')
         return JsonResponse(dict())
 
 
